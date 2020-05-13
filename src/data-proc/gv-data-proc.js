66077758bb1b7e5f7efb4eb33267ec27abcacd19
Dave Beyer <dave.beyer@gmail.com>
2020-05-12 18:10:28 -0700

    Add debugLevel support in GVDataProcessor

--- src/data-proc/gv-data-proc.js
+++ src/data-proc/gv-data-proc.js
@@ -3,7 +3,9 @@ import HtmlDataProcessor from  '@ckeditor/ckeditor5-engine/src/dataprocessor/htm
 import { convertMetaImgsToView } from '../lib';
 
 export default class GVDataProcessor {
+
     constructor(editor) {
+        this._dbgLevel = editor.config.get('debugLevel');
         this._htmlProc = new HtmlDataProcessor(editor.data.viewDocument);
     }
 
@@ -16,7 +18,10 @@ export default class GVDataProcessor {
         // Convert metaimg tags to tags like <span
         // class="gv-metatag">... </span> to avoid CKEditor5 core
         // conversions from changing <img> tags to <figure><img>...
-        console.log("DataProcessor.toView pre:", data.toString());
+
+        if (this._dbgLevel > 5) {
+            console.log("DataProcessor.toView pre:", data.toString());
+        }
 
         let newData = convertMetaImgsToView(data);
 
@@ -24,18 +29,22 @@ export default class GVDataProcessor {
         // Now use original HTMLDataProcessor's toView() 
         //
 
-        if (newData) {
-            console.log("DataProcessor.toView post:", newData.toString());
-        } else {
-            console.log("DataProcessor.toView post: null!?");
+        if (this._dbgLevel > 5) {
+            if (newData) {
+                console.log("DataProcessor.toView post:", newData.toString());
+            } else {
+                console.log("DataProcessor.toView post: null!?");
+            }
         }
 
         let viewData = this._htmlProc.toView(newData);
 
-        if (viewData) {
-            console.log("DataProcessor.toView done:", viewData.toString());
-        } else {
-            console.log("DataProcessor.toView done: null!?");
+        if (this._dbgLevel > 5) {
+            if (viewData) {
+                console.log("DataProcessor.toView done:", viewData.toString());
+            } else {
+                console.log("DataProcessor.toView done: null!?");
+            }
         }
 
         return viewData;
